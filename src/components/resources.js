2eb9f13d51d653cccd03777294eccd89cc27a601
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-04 12:18:28 -0700

    Add fetchMessage and render message

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchMessage } from '../actions'
 
-export default class Resources extends Component {
+class Resources extends Component {
+  componentDidMount(){
+    this.props.fetchMessage()
+  }
+
   render(){
+    console.log(this.props.message);
+
     return (
       <div>
         <h4>Resources</h4>
-        <div> Hi </div>
+        <div>{this.props.message}</div>
       </div>
     )
   }
 }
 
+function mapStateToProps(state){
+  return { message: state.auth.message }
+}
 
+export default connect(mapStateToProps, { fetchMessage })(Resources)
 // we pass this in to a higher order component
 // to give it some additional behavior
 // so that it becomes a wrapped Resources
