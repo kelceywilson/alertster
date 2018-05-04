ba45ee0d09872064de30e28dc9171e1de74dd4a4
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-04 11:34:08 -0700

    Add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReduxPromise from 'redux-promise'
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+// import ReactDOM from 'react-dom';
+// import ReduxPromise from 'redux-promise'
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
-import reducers from './reducers';
+// import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-    <App />
-  </Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  console.log(wrapper.debug());
+  expect(wrapper).toBeTruthy();
 });
+
+
+
+
+// const div = document.createElement('div');
+// ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+//   <App />
+// </Provider>, div);
+// ReactDOM.unmountComponentAtNode(div);
