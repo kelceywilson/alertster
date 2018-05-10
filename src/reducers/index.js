52317c6c6e8aa632bf2a2babd2c77a740eb163fa
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-10 15:14:19 -0700

    Add file: FileUploadReducer to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import AlertsReducer from './alerts_reducer'
 import SelectAlertReducer from './select_alert_reducer'
 import AuthReducer from './auth_reducer'
 import ModalReducer from './modal_reducer'
+import FileUploaderReducer from './file_uploader_reducer'
 
 const rootReducer = combineReducers({
   alerts: AlertsReducer,
   auth: AuthReducer,
   selectAlert: SelectAlertReducer,
   form: FormReducer,
-  open: ModalReducer
+  open: ModalReducer,
+  file: FileUploaderReducer
 });
 
 export default rootReducer
