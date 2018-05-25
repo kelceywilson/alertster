33de55a1ce962c1e50ea9c71014362b9d03ef3c9
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-25 09:50:53 -0700

    Remove debugging console logs

--- src/reducers/file_uploader_reducer.js
+++ src/reducers/file_uploader_reducer.js
@@ -1,11 +1,11 @@
-import { UPLOAD_FILE } from '../actions/index'
+import { CLOSE_MODAL, UPLOAD_FILE } from '../actions/index'
 
 export default function(state = {}, action){
-  // console.log('action received', action)
   switch (action.type) {
   case UPLOAD_FILE:
-    console.log('file_uploader_reducer', action.payload);
     return { ...state, photo_url: action.payload }
+  case CLOSE_MODAL:
+    return {...state, photo_url: null }
   default:
     return state
   }
