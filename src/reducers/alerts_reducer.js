a295431b07349fd16a63e9e3ac20c741b455f940
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-25 09:49:31 -0700

    Make mapKeys into function

--- src/reducers/alerts_reducer.js
+++ src/reducers/alerts_reducer.js
@@ -1,17 +1,19 @@
 import _ from 'lodash'
 import { ADD_NEW_ALERT, DELETE_ALERT, GET_ALL_ALERTS, SEARCH_ALERTS, SELECT_ALERT } from '../actions/index'
 
+const mappedAlerts = action => _.mapKeys(action.payload.data, '_id')
+
 export default function(state = [], action){
   // console.log('action received', action)
   switch (action.type) {
   case ADD_NEW_ALERT:
-    return _.mapKeys(action.payload.data, '_id')
+    return mappedAlerts(action)
   case GET_ALL_ALERTS:
-    return _.mapKeys(action.payload.data, '_id')
+    return mappedAlerts(action)
   case DELETE_ALERT:
-    return _.mapKeys(action.payload.data, '_id')
+    return mappedAlerts(action)
   case SEARCH_ALERTS:
-    return _.mapKeys(action.payload.data, '_id')
+    return mappedAlerts(action)
   case SELECT_ALERT:
     return state
   default:
