5df57c032fd7d30f2bf3af5cc3fbabe6133561d6
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-25 09:51:56 -0700

    Add alert_type to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { reducer as FormReducer } from 'redux-form'
 import AlertsReducer from './alerts_reducer'
+import SetAlertTypeReducer from './set_alert_type_reducer'
 import SelectAlertReducer from './select_alert_reducer'
 import AuthReducer from './auth_reducer'
 import ModalReducer from './modal_reducer'
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
   selectAlert: SelectAlertReducer,
   form: FormReducer,
   open: ModalReducer,
-  file: FileUploaderReducer
+  file: FileUploaderReducer,
+  alert_type: SetAlertTypeReducer
 });
 
 export default rootReducer
