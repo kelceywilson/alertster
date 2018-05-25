7fa18325eb255b50be87c78eb1b02834c717ca9f
Kelcey Wilson <kelceywilson@gmail.com>
2018-05-25 09:31:22 -0700

    Organize into categories

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,25 +2,115 @@ import axios from 'axios'
 
 export const ADD_NEW_ALERT = 'ADD_NEW_ALERT'
 export const ADD_NEW_USER = 'ADD_NEW_USER'
+export const AUTH_ERROR = 'AUTH_ERROR'
+export const AUTH_USER = 'AUTH_USER'
 export const CHANGE_AUTH = 'CHANGE_AUTH'
+export const CLOSE_MODAL = 'CLOSE_MODAL'
 export const DELETE_ALERT = 'DELETE_ALERT'
 export const FETCH_MESSAGE = 'FETCH_MESSAGE'
 export const GET_ALL_ALERTS = 'GET_ALL_ALERTS'
 export const GET_DETAILS = 'GET_DETAILS'
+export const OPEN_MODAL = 'OPEN_MODAL'
 export const SEARCH_ALERTS = 'SELECT_ALERT'
 export const SELECT_ALERT = 'SELECT_ALERT'
-export const UPDATE_TOKEN = 'UPDATE_TOKEN'
-export const AUTH_USER = 'AUTH_USER'
-export const AUTH_ERROR = 'AUTH_ERROR'
-export const UNAUTH_USER = 'UNAUTH_USER'
+export const SET_ALERT_TYPE = 'SET_ALERT_TYPE'
 export const SIGN_OUT = 'SIGN_OUT'
-export const OPEN_MODAL = 'OPEN_MODAL'
-export const CLOSE_MODAL = 'CLOSE_MODAL'
+export const UNAUTH_USER = 'UNAUTH_USER'
+export const UPDATE_TOKEN = 'UPDATE_TOKEN'
 export const UPLOAD_FILE = 'UPLOAD_FILE'
 
 const ROOT_URL = 'http://localhost:5000'
 // const ROOT_URL = 'https://mighty-castle-33351.herokuapp.com'
 
+// ALERT ACTIONS //
+export function addNewAlert(values){
+  console.log('addNewAlert', values);
+  const request = axios.post(`${ROOT_URL}/alerts`, values)
+  return {
+    type: ADD_NEW_ALERT,
+    payload: request
+  }
+}
+export function deleteAlert(alertId){
+  console.log('deleteAlert', alertId);
+  const request = axios.delete(`${ROOT_URL}/alerts/${alertId}`)
+  return {
+    type: DELETE_ALERT,
+    payload: request
+  }
+}
+export function filterAlerts(filter){
+  console.log('filterAlerts', filter);
+  const request = axios.get(`${ROOT_URL}/alerts/filter?filterBy=${filter}`)
+  return {
+    type: SEARCH_ALERTS,
+    payload: request
+  }
+}
+export function getAllAlerts(){
+  const request = axios.get(`${ROOT_URL}/alerts`)
+  return {
+    type: GET_ALL_ALERTS,
+    payload: request
+  }
+}
+export function getDetails(alertId){
+  return {
+    type: GET_DETAILS,
+    payload: alertId
+  }
+}
+export function searchAlerts(terms){
+  console.log('searchAlerts', terms);
+  const request = axios.get(`${ROOT_URL}/alerts/search?terms=${terms}`)
+  return {
+    type: SEARCH_ALERTS,
+    payload: request
+  }
+}
+export function selectAlert(alertId){
+  console.log('selectAlert', alertId);
+  return {
+    type: SELECT_ALERT,
+    payload: alertId
+  }
+}
+export function setAlertType(alertType){
+  console.log('setAlertType', alertType);
+  return {
+    type: SET_ALERT_TYPE,
+    payload: alertType
+  }
+}
+export const uploadFile = (event) => {
+  const file = event.target.files[0]
+  const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/alertsapi/upload'
+  const CLOUDINARY_UPLOAD_PRESET = 'p2egownn'
+  const formData = new FormData()
+  formData.append('file', file)
+  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+  return (dispatch) => {
+    return axios({
+      url: CLOUDINARY_URL,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: formData
+    }).then((response) => {
+      // console.log('cloudinary response', response);
+      dispatch({
+        type: UPLOAD_FILE,
+        payload: response.data.secure_url
+      })
+      // return response.data.secure_url
+    }).catch(function(err){
+      console.log(err);
+    })
+  }
+}
+
+// AUTHENTICATION //
 export function authenticate(isLoggedIn){
   return {
     type: CHANGE_AUTH,
@@ -62,9 +152,6 @@ export function signinUser({ email, password }, callback){
         // - redirect to the route desired
         // old way
         // - browserHistory.push('/')
-        // tried:
-        //    return <Redirect to='/' push={true} />
-        //    store.dispatch(push('/'))
         callback()
       })
       .catch((response) => {
@@ -103,26 +190,10 @@ export function signoutUser(){
     type: UNAUTH_USER
   }
 }
-export function addNewAlert(values){
-  console.log('addNewAlert', values);
-  const request = axios.post(`${ROOT_URL}/alerts`, values)
-  return {
-    type: ADD_NEW_ALERT,
-    payload: request
-  }
-}
 export function addNewUser(newUser){
   return {
     type: ADD_NEW_USER,
     payload: newUser
-  }
-}
-export function deleteAlert(alertId){
-  console.log('deleteAlert', alertId);
-  const request = axios.delete(`${ROOT_URL}/alerts/${alertId}`)
-  return {
-    type: DELETE_ALERT,
-    payload: request
   }
 }
 // export function facebookLogin(){
@@ -134,43 +205,23 @@ export function deleteAlert(alertId){
 //     payload: request
 //   }
 // }
-export function getAllAlerts(){
-  const request = axios.get(`${ROOT_URL}/alerts`)
+
+
+// MODAL ACTIONS //
+export function openModal(){
   return {
-    type: GET_ALL_ALERTS,
-    payload: request
+    type: OPEN_MODAL,
+    payload: true
   }
 }
-export function getDetails(alertId){
+export function closeModal(){
   return {
-    type: GET_DETAILS,
-    payload: alertId
-  }
-}
-export function filterAlerts(filter){
-  console.log('searchAlerts', filter);
-  const request = axios.get(`${ROOT_URL}/alerts/filter?filterBy=${filter}`)
-  return {
-    type: SEARCH_ALERTS,
-    payload: request
-  }
-}
-export function searchAlerts(terms){
-  console.log('searchAlerts', terms);
-  const request = axios.get(`${ROOT_URL}/alerts/search?terms=${terms}`)
-  return {
-    type: SEARCH_ALERTS,
-    payload: request
-  }
-}
-export function selectAlert(alertId){
-  console.log(alertId);
-  return {
-    type: SELECT_ALERT,
-    payload: alertId
+    type: CLOSE_MODAL,
+    payload: false
   }
 }
 
+// USER ACTIONS? //
 // redux thunk version of fetchMessage
 export function fetchMessage(){
   return function(dispatch){
@@ -186,7 +237,6 @@ export function fetchMessage(){
       })
   }
 }
-
 // redux promise version
 // export function fetchMessage(){
 //   const request = axios.get(ROOT_URL, {
@@ -198,52 +248,3 @@ export function fetchMessage(){
 //     payload: request.message
 //   }
 // }
-
-
-export function openModal(){
-  return {
-    type: OPEN_MODAL,
-    payload: true
-  }
-}
-
-export function closeModal(){
-  return {
-    type: CLOSE_MODAL,
-    payload: false
-  }
-}
-
-export const uploadFile = (event) => {
-  const file = event.target.files[0]
-  const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/alertsapi/upload'
-  const CLOUDINARY_UPLOAD_PRESET = 'p2egownn'
-  const formData = new FormData()
-  formData.append('file', file)
-  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-  return (dispatch) => {
-    return axios({
-      url: CLOUDINARY_URL,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: formData
-    }).then((response) => {
-      console.log('cloudinary response', response);
-      dispatch({
-        type: UPLOAD_FILE,
-        payload: response.data.secure_url
-      })
-      // return response.data.secure_url
-    }).catch(function(err){
-      console.log(err);
-    })
-  }
-}
-// const fileURL = 'http://fillmurray.com/g/100/100'
-
-// dispatch({
-//   type: UPLOAD_FILE,
-//   payload: response.data.secure_url
-// })
