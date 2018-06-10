a4c206ca177ceee74f05d325ee2d7f392c0a2f2c
Kelcey Wilson <kelceywilson@gmail.com>
2018-06-10 11:56:40 -0700

    Change open state to object to include additional payloads

--- src/reducers/modal_reducer.js
+++ src/reducers/modal_reducer.js
@@ -1,11 +1,14 @@
-import { CLOSE_MODAL, OPEN_MODAL } from '../actions/index'
+import { CLOSE_MODAL, DELETE_ALERT, OPEN_MODAL } from '../actions/index'
 
 export default function(state = {open: false}, action){
   switch (action.type) {
   case CLOSE_MODAL:
     return {open: false}
+  case DELETE_ALERT:
+    return {open: false}
   case OPEN_MODAL:
-    return {whichModal: action.payload, open: true}
+    console.log('OPEN_MODAL', action.payload);
+    return {...action.payload, open: true}
   default:
     return state
   }
