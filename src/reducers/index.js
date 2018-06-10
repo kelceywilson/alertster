55f263a698ae12b6ee159738120d4ebd81c5b9f6
Kelcey Wilson <kelceywilson@gmail.com>
2018-06-10 11:54:27 -0700

    Add filtered state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as FormReducer } from 'redux-form'
 import AlertsReducer from './alerts_reducer'
 import SetAlertTypeReducer from './set_alert_type_reducer'
-import SelectAlertReducer from './select_alert_reducer'
+import FilteredReducer from './filtered_reducer'
 import AuthReducer from './auth_reducer'
 import ModalReducer from './modal_reducer'
 import FileUploaderReducer from './file_uploader_reducer'
@@ -10,7 +10,7 @@ import FileUploaderReducer from './file_uploader_reducer'
 const rootReducer = combineReducers({
   alerts: AlertsReducer,
   auth: AuthReducer,
-  selectAlert: SelectAlertReducer,
+  filtered: FilteredReducer,
   form: FormReducer,
   open: ModalReducer,
   file: FileUploaderReducer,
