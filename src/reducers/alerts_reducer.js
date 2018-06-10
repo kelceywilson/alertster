f51d513af6214134580d27ab558166d4082ece69
Kelcey Wilson <kelceywilson@gmail.com>
2018-06-10 11:58:02 -0700

    Change alerts state to object to include additional payloads

--- src/reducers/alerts_reducer.js
+++ src/reducers/alerts_reducer.js
@@ -1,23 +1,22 @@
 import _ from 'lodash'
-import { ADD_NEW_ALERT, DELETE_ALERT, GET_ALL_ALERTS, GET_ONE_ALERT, SEARCH_ALERTS, SELECT_ALERT } from '../actions/index'
+import { ADD_NEW_ALERT, DELETE_ALERT, GET_ALL_ALERTS, GET_ONE_ALERT, FILTER_ALERTS } from '../actions/index'
 
-const mappedAlerts = action => _.mapKeys(action.payload.data, '_id')
+// const mappedAlerts = action => _.mapKeys(action.payload.data, '_id')
 
-export default function(state = {filtered: false, list: []}, action){
+export default function(state = {list: [], alert: {}}, action){
   // console.log('action received', action)
   switch (action.type) {
   case ADD_NEW_ALERT:
-    return {filtered: false, list: mappedAlerts(action)}
-  case GET_ALL_ALERTS:
-    return {filtered: false, list: mappedAlerts(action)}
-  case GET_ONE_ALERT:
-    return {filtered: false, list: mappedAlerts(action)}
+    return {list: _.mapKeys(action.payload.data, '_id')}
   case DELETE_ALERT:
-    return {filtered: false, list: mappedAlerts(action)}
-  case SEARCH_ALERTS:
-    return {filtered: true, list: mappedAlerts(action)}
-  case SELECT_ALERT:
-    return state
+    return {list: _.mapKeys(action.payload.data, '_id')}
+  case FILTER_ALERTS:
+    return {list: _.mapKeys(action.payload.data, '_id')}
+  case GET_ALL_ALERTS:
+    return {list: _.mapKeys(action.payload.data, '_id')}
+  case GET_ONE_ALERT:
+    console.log(action.payload.data);
+    return {...state, alert: action.payload.data}
   default:
     return state
   }
