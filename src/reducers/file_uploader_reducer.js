8dcf4b3a2a8248c2ac8283bd03387cd4836aedb6
Kelcey Wilson <kelceywilson@gmail.com>
2018-06-16 13:03:52 -0700

    Put photo_url into store on OPEN_MODAL

--- src/reducers/file_uploader_reducer.js
+++ src/reducers/file_uploader_reducer.js
@@ -1,9 +1,11 @@
-import { CLOSE_MODAL, DELETE_PHOTO_URL, UPLOAD_FILE } from '../actions/index'
+import { CLOSE_MODAL, DELETE_PHOTO_URL, OPEN_MODAL, UPLOAD_FILE } from '../actions/index'
 
 export default function(state = {}, action){
   switch (action.type) {
   case UPLOAD_FILE:
     return {...state, photo_url: action.payload}
+  case OPEN_MODAL:
+    return {...state, photo_url: action.payload.photo_url}
   case CLOSE_MODAL:
     return {...state, photo_url: null}
   case DELETE_PHOTO_URL:
